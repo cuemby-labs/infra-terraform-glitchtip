bc8702488900d888f2e2060f7f5e0b282f8917e9
chgaviria <christian.gaviria@cuemby.com>
2025-02-17 10:30:48 -0500

    fix minio variables

--- main.tf
+++ main.tf
@@ -25,11 +25,11 @@ resource "helm_release" "glitchtip" {
       REDIS_URL                 = var.redis_url,
       EMAIL_URL                 = var.email_url,
       DEFAULT_FROM_EMAIL        = var.emailfrom_url,
-      DEFAULT_FILE_STORAGE      = var.default_file_storage
-      AWS_ACCESS_KEY_ID         = var.aws_access_key_id
-      AWS_SECRET_ACCESS_KEY     = var.aws_secret_access_key
-      AWS_STORAGE_BUCKET_NAME   = var.aws_storage_bucket_name
-      AWS_S3_ENDPOINT_URL       = var.aws_s3_endpoint_url
+      DEFAULT_FILE_STORAGE      = var.default_file_storage,
+      AWS_ACCESS_KEY_ID         = var.aws_access_key_id,
+      AWS_SECRET_ACCESS_KEY     = var.aws_secret_access_key,
+      AWS_STORAGE_BUCKET_NAME   = var.aws_storage_bucket_name,
+      AWS_S3_ENDPOINT_URL       = var.aws_s3_endpoint_url,
       web_request_memory        = var.resources["web"]["requests"]["memory"],
       web_limits_memory         = var.resources["web"]["limits"]["memory"],
       web_request_cpu           = var.resources["web"]["requests"]["cpu"],
